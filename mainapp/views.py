2c7fbb76dc3bf42d939a298ee62dabf66309ed6a
Ex1le <3x1le@rambler.ru>
2023-01-14 18:01:31 +0300

    lesson_5 New app and user model

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
-
 from mainapp import models as mainapp_models
 
 
@@ -42,12 +41,9 @@ class CoursesDetailView(TemplateView):
 
     def get_context_data(self, pk=None, **kwargs):
         context = super(CoursesDetailView, self).get_context_data(**kwargs)
-        context["course_object"] = get_object_or_404(
-            mainapp_models.Courses, pk=pk)
-        context["lessons"] = mainapp_models.Lesson.objects.filter(
-            course=context["course_object"])
-        context["teachers"] = mainapp_models.CourseTeachers.objects.filter(
-            course=context["course_object"])
+        context["course_object"] = get_object_or_404(mainapp_models.Courses, pk=pk)
+        context["lessons"] = mainapp_models.Lesson.objects.filter(course=context["course_object"])
+        context["teachers"] = mainapp_models.CourseTeachers.objects.filter(course=context["course_object"])
         return context
 
 
